9bf6e781a2c3be0d7d8fc8e34e1c7fbb37b8e5e4
Bill Jason <jasonez_03@yahoo.com>
2019-12-03 14:07:45 -0800

    added mongoDB

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+const auth = require(".assets/auth.js")
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const http = require("http").Server(app);
 const port = 3000;
 http.listen(port);
 console.log("The Express server is running on port " + port + "!");
-app.use("/cannotget", express.static("./web_files") );
+console.log(auth.getDBURL());
 
+
+//routes
 app.use(express.static("client/"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post("/login", (request, response) => {
+  let requestUsername = request.body.username;
+  let requestPassword = request.body.password;
+
+  console.log(requestUsername,requestPassword);
+  response.sendStatus(200);
+});
 
