d88413069e9bbb93b59bb889038505e8502b92cd
Bill Jason <jasonez_03@yahoo.com>
2019-12-03 10:27:03 -0800

    added jquery + form + script via index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//const fs = require("fs");
-//const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
@@ -7,4 +5,6 @@ const port = 3000;
 http.listen(port);
 console.log("The Express server is running on port " + port + "!");
 app.use("/cannotget", express.static("./web_files") );
+
+app.use(express.static("client/"));
 
